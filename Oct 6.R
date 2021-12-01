8d1b47e3047b51c2504270e2e72a5c5fc4374e59
LydiGool <93364215+LydiGool@users.noreply.github.com>
2021-12-01 12:56:41 -0500

    Add files via upload

---
+++ Oct 6.R
@@ -0,0 +1,7 @@
+# Author: Goolsby, Date: Oct 6 2021, Purpose: Calculate the probability distribution values
+
+
+
+# Calculate Continuous Uniform distribution probability 
+
+runif(10, min=1, max=3)
